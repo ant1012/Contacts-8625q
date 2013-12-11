f51195971f9ed731afcafa006d2badcc440944ad
x7 <x7.0@outlook.com>
2013-12-11 17:23:54 +0800

    svn

--- src/edu/bupt/contacts/callinfo/CallinfoActivity.java
+++ src/edu/bupt/contacts/callinfo/CallinfoActivity.java
@@ -2,17 +2,22 @@ package edu.bupt.contacts.callinfo;
 
 import java.io.InputStream;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import com.android.internal.telephony.msim.ITelephonyMSim;
 
 import edu.bupt.contacts.R;
+import edu.bupt.contacts.observer.ContactsCacheDBHelper;
+import a_vcard.android.content.ContentValues;
 import android.app.Activity;
 import android.content.ContentResolver;
 import android.content.ContentUris;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
@@ -23,9 +28,12 @@ import android.os.Handler;
 import android.os.Message;
 import android.os.RemoteException;
 import android.os.ServiceManager;
+import android.preference.PreferenceManager;
 import android.provider.CallLog;
 import android.provider.Contacts.People;
 import android.provider.ContactsContract;
+import android.telephony.PhoneNumberFormattingTextWatcher;
+import android.telephony.TelephonyManager;
 import android.text.Html;
 import android.text.Html.ImageGetter;
 import android.text.Spanned;
@@ -50,8 +58,16 @@ public class CallinfoActivity extends Activity {
     private Button buttonMsg;
     private Button buttonDial;
     private Button buttonAdd;
-    String number = "";
-    String name = "";
+    private long id = -1;
+    private String number = "";
+    private String name = "";
+    private String numberCut10= "";
+    private String numberCut11 = "";  //原始后十一位电话号码
+    private String numberCutFormat11 =""; //格式化的后十一位电话号码
+    private String numberCutFormat10 = "";//格式化的后十位位电话号码，因为添加了横杠，实际有十三位
+    private String strPhoneNumber;
+    private String numberBlank10;
+    private Context context;
     static public boolean exist = false;
 
     @Override
@@ -71,13 +87,98 @@ public class CallinfoActivity extends Activity {
         Cursor cursor = getContentResolver().query(CallLog.Calls.CONTENT_URI, null, null, null, "date desc limit 1");
         cursor.moveToFirst();
 
-        long id = cursor.getLong(cursor.getColumnIndex(CallLog.Calls._ID));
+        id = cursor.getLong(cursor.getColumnIndex(CallLog.Calls._ID));
         Log.v(TAG, "id - " + id);
         number = cursor.getString(cursor.getColumnIndex(CallLog.Calls.NUMBER));
         Log.v(TAG, "number - " + number);
+        Log.v(TAG,"raw_number_format - "+fomatNumber(number));
         name = cursor.getString(cursor.getColumnIndex(CallLog.Calls.CACHED_NAME));
         Log.v(TAG, "name - " + name);
         cursor.close();
+        
+        /**
+         * ddd 取电话号码后十一位，格式化之后，取后十三位
+         * */
+    	
+        // 获取TelephonyManager用于判断是否漫游
+        TelephonyManager tm = (TelephonyManager) getSystemService(Context.TELEPHONY_SERVICE);
+        // 获取SharedPreferences用于判断是否开启漫游测试
+        SharedPreferences sp = PreferenceManager.getDefaultSharedPreferences(this);
+        
+        if(tm.isNetworkRoaming() || sp.getBoolean("RoamingTestPreference", false)){
+        	Log.v(TAG, "tm.isNetworkRoaming()");
+        	if(number.length()<11){
+        		numberCut10 = number;
+        		numberBlank10 = fomatBlankNumber(number);
+        		numberCutFormat10 = fomatNumber(number);
+        		Log.v(TAG,"numberCutFormat10_less_than_11"+numberCutFormat10);
+            }
+            else{
+            	
+            numberCut10=number.substring(number.length()-10,number.length());
+            numberCut11 = number.substring(number.length()-11,number.length());
+            Log.v(TAG, "numberCut11 - " + numberCut11);
+            numberCutFormat11 = fomatNumber(numberCut11);
+            Log.v(TAG, "numberCutFormat11 - " + numberCutFormat11);
+            Log.v(TAG,"fomatBlankNumber(numberCut11)"+fomatBlankNumber(numberCut11));
+            numberBlank10 = fomatBlankNumber(numberCut11).substring(fomatBlankNumber(numberCut11).length()-12,fomatBlankNumber(numberCut11).length());
+            numberCutFormat10 = numberCutFormat11.substring(numberCutFormat11.length()-13,numberCutFormat11.length());
+            Log.v(TAG, "numberCutFormat10 - " + numberCutFormat10);
+            }
+        	
+            Log.v(TAG,"numberCut10--"+numberCut10);
+            Log.v(TAG,"numberBlank10--"+numberBlank10);
+
+            Map<String, String> map =  getContactidFromCutNumber(numberCut10,numberCutFormat10,numberBlank10);
+            
+           Log.v(TAG,"map--"+map);
+           
+           if(map.size() != 0){
+        	   //name = map.get("displayName");
+        	   Log.v(TAG,"displayName--"+name);
+        	   //number = map.get("contactNumber");
+        	   Log.v(TAG,"contactNumber--"+number);
+        	   modifyCallLog(id,map.get("displayName"),map.get("contactNumber"));
+           } 
+
+        	
+        	
+        }
+        
+
+       
+//        Cursor phonecur = null;
+//        phonecur = getContentResolver().query(
+//                android.provider.ContactsContract.CommonDataKinds.Phone.CONTENT_URI,null,
+//                android.provider.ContactsContract.CommonDataKinds.Phone.NUMBER+"LIKE '%"+numberCutFormat10+"'",           
+//                null, null);
+//        // get number
+//        Log.v(TAG,android.provider.ContactsContract.CommonDataKinds.Phone.CONTENT_URI.toString());
+//        while (phonecur.moveToNext()) {
+//            strPhoneNumber = phonecur.getString(phonecur
+//                    .getColumnIndex(android.provider.ContactsContract.CommonDataKinds.Phone.NUMBER));
+//            Log.v(TAG,"strPhoneNumber--"+strPhoneNumber);
+            // if (strPhoneNumber.length() > 4)
+            // contactsList.add("18610011001" + "\n测试");
+            // contactsList.add(strPhoneNumber+"\n"+name+"");
+
+            // Log.i(TAG, "strPhoneNumber - " + strPhoneNumber);
+
+//            Map<String, String> map = new HashMap<String, String>();
+//            map.put("id", contactId);
+//            map.put("name", name);
+//            map.put("number", strPhoneNumber);
+//            contactsCacheDBHelper.addLine(contactId, name, strPhoneNumber);
+
+            // list.add(map);
+//            soManyLines++;
+//          }
+//        phonecur.close();
+
+        
+
+        
+        
 
         ArrayList<String> contactids = getContactidFromNumber(number);
 
@@ -157,7 +258,8 @@ public class CallinfoActivity extends Activity {
         Log.d(TAG, "getContactidFromNumber");
         ArrayList<String> contactidList = new ArrayList<String>();
         Cursor pCur = getContentResolver().query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI, null,
-                ContactsContract.CommonDataKinds.Phone.NUMBER + " = ?", new String[] { phoneNumber }, null);
+                ContactsContract.CommonDataKinds.Phone.NUMBER + " = ?",        		
+        		new String[] { phoneNumber }, null);
         while (pCur.moveToNext()) {
             contactidList.add(pCur.getString(pCur.getColumnIndex(ContactsContract.CommonDataKinds.Phone.CONTACT_ID)));
         }
@@ -198,6 +300,79 @@ public class CallinfoActivity extends Activity {
         return contactidList;
     }
 
+    //ddd 通过截短的十位电话找到contact_id
+//    private ArrayList<String> getContactidFromCutNumber(String phoneNumber) {
+//        Log.d(TAG, "getContactidFromCutNumber");
+//        ArrayList<String> contactidList = new ArrayList<String>();
+//        Cursor pCur = getContentResolver().query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI, null,
+//        		android.provider.ContactsContract.CommonDataKinds.Phone.NUMBER+" LIKE '%"+phoneNumber+"'",
+//        		null, null);
+//        while (pCur.moveToNext()) {
+//            contactidList.add(pCur.getString(pCur.getColumnIndex(ContactsContract.CommonDataKinds.Phone.CONTACT_ID)));
+//         //   contactidList.add(pCur.getString(pCur.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER)));
+//        }
+//        pCur.close();
+//
+//        // -
+//        Cursor pCurFormat = getContentResolver()
+//                .query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI, null,
+//                        //ContactsContract.CommonDataKinds.Phone.NUMBER + " = ?",
+//                		android.provider.ContactsContract.CommonDataKinds.Phone.NUMBER+" LIKE '%"+phoneNumber+"'",
+//                		null, null);
+//        while (pCurFormat.moveToNext()) {
+//            contactidList.add(pCurFormat.getString(pCurFormat
+//                    .getColumnIndex(ContactsContract.CommonDataKinds.Phone.CONTACT_ID)));
+//    //        contactidList.add(pCur.getString(pCur.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER)));
+//        }
+//        pCurFormat.close();
+//        return contactidList;
+//    }
+    
+    private Map<String, String> getContactidFromCutNumber(String phoneNumber,String formatNmuber,String formatBlankNmuber) {
+        Log.i(TAG, "phone-number--"+phoneNumber);
+        Map<String, String> map = new HashMap<String, String>();
+        Cursor pCur = getContentResolver().query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI, null,
+        		android.provider.ContactsContract.CommonDataKinds.Phone.NUMBER+" LIKE '%"+phoneNumber+"'",
+        		null, null);
+        while (pCur.moveToNext()) {
+            map.put("contactId",pCur.getString(pCur.getColumnIndex(ContactsContract.CommonDataKinds.Phone.CONTACT_ID)));
+            map.put("contactNumber",pCur.getString(pCur.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER)));
+            map.put("displayName",pCur.getString(pCur.getColumnIndex(ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME)));
+        Log.v(TAG,"ddd-pur-number--"+pCur.getString(pCur.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER))); 
+        }
+        
+        pCur.close();
+
+        // -
+        Cursor pCurFormat = getContentResolver()
+                .query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI, null,
+                        //ContactsContract.CommonDataKinds.Phone.NUMBER + " = ?",
+                		android.provider.ContactsContract.CommonDataKinds.Phone.NUMBER+" LIKE '%"+formatNmuber+"'",
+                		null, null);
+        while (pCurFormat.moveToNext()) {
+            map.put("contactId",pCurFormat.getString(pCurFormat
+                    .getColumnIndex(ContactsContract.CommonDataKinds.Phone.CONTACT_ID)));
+            map.put("contactNumber",pCurFormat.getString(pCurFormat.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER)));
+            map.put("displayName",pCurFormat.getString(pCurFormat.getColumnIndex(ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME)));
+            Log.i(TAG,"ddd-purformat-number--"+pCurFormat.getString(pCur.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER))); 
+        }
+        pCurFormat.close();
+        
+        Cursor pCurBlank = getContentResolver().query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI, null,
+        		android.provider.ContactsContract.CommonDataKinds.Phone.NUMBER+" LIKE '%"+formatBlankNmuber+"'",
+        		null, null);
+        while (pCurBlank.moveToNext()) {
+            map.put("contactId",pCurBlank.getString(pCurBlank.getColumnIndex(ContactsContract.CommonDataKinds.Phone.CONTACT_ID)));
+            map.put("contactNumber",pCurBlank.getString(pCurBlank.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER)));
+            map.put("displayName",pCurBlank.getString(pCurBlank.getColumnIndex(ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME)));
+        Log.v(TAG,"ddd-pCurBlank-number--"+pCurBlank.getString(pCurBlank.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER))); 
+        }
+        
+        pCur.close();
+        return map;
+    }
+    
+    
     //add '-' to input number, match the format in DB
     private String fomatNumber(String input) {
         if (input.startsWith("1")) {
@@ -222,6 +397,35 @@ public class CallinfoActivity extends Activity {
         }
         return "";
     }
+
+    private String fomatBlankNumber(String input) {
+//        if (input.startsWith("1")) {
+//            if (input.length() == 1) {
+//                return input;
+//            } else if (input.length() > 1 && input.length() < 5) {
+//                return input.substring(0, 1) + "-" + input.substring(1, input.length());
+//            } else if (input.length() >= 5 && input.length() < 8) {
+//                return input.substring(0, 1) + "-" + input.substring(1, 4) + "-" + input.substring(4, input.length());
+//            } else if (input.length() >= 8) {
+//                return input.substring(0, 1) + "-" + input.substring(1, 4) + "-" + input.substring(4, 7) + "-"
+//                        + input.substring(7, input.length());
+//            }
+//        } else {
+            if (input.length() <= 3) {
+                return input;
+            } else if (input.length() > 3 && input.length() < 7) {
+                return input.substring(0, 3) + " " + input.substring(3, input.length());
+            } else if (input.length() >= 7 && input.length() <= 11 ) {
+                return input.substring(0, 3) + " " + input.substring(3, 7) + " " + input.substring(7, input.length());
+            }
+            else if(input.length()>11){
+            	return input;
+            	
+            }
+//        }
+        return "";
+    }
+    
 
     private String replacePattern(String origin, String pattern, String replace) {
         Log.i(TAG, "origin - " + origin);
@@ -288,4 +492,29 @@ public class CallinfoActivity extends Activity {
         exist = false;
         super.onDestroy();
     }
+    
+    
+    
+    
+    private void modifyCallLog(long id2,String name,String number){
+    	android.content.ContentValues content = new android.content.ContentValues();
+    	content.put(CallLog.Calls.NUMBER, number);
+    	content.put(CallLog.Calls.CACHED_NAME,name);
+    	getContentResolver().update(CallLog.Calls.CONTENT_URI, content, CallLog.Calls._ID+"="+id2, null);
+    	
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
 }
